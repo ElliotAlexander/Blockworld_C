f4a9adfd0aab1b22bc37957c5f49748a556b5d31
Elliot Alexander <epurvis97@gmail.com>
2017-10-21 23:41:53 +0100

    Added checks for agent and tile being in the same location.

--- Main.c
+++ Main.c
@@ -29,10 +29,10 @@ void generate_args(int argc, char** argv){
 			// position of agent
 			case 'a':
 			{
-				int x,y, state_val;
-				state_val = sscanf(strtok(optarg, ","), "%d", &x);
-				state_val = state_val && (sscanf(strtok(NULL, ","), "%d", &y));
-				printf("Parsed agent position of [%d,%d] | return state %d\n", x, y, state_val);
+				int state_val;
+				state_val = sscanf(strtok(optarg, ","), "%d", &g_agent.x);
+				state_val = state_val && (sscanf(strtok(NULL, ","), "%d", &g_agent.y));
+				printf("Parsed agent position of [%d,%d] | return state %d\n", g_agent.x, g_agent.y, state_val);
 				break;
 			}
 
@@ -69,11 +69,17 @@ void generate_args(int argc, char** argv){
 
 struct Blockboard generate_blockboard(void){
 	printf("Generating new block board.\n");
+
+	// Basic parameters. 
 	struct Blockboard new_board;
 	new_board.N = N_arg;
 	new_board.i_max = N_arg*N_arg;
+
+	// Add tiles. 
 	for(int i = 0; i < 4096; i++){
 		if(g_tile_arr[i].initialised == 1){
+
+			//  We copy data directly so we can reset the blockboard later. 
 			struct Tuple t;
 			t.initialised = 1;
 			t.x = g_tile_arr[i].x;
@@ -84,6 +90,19 @@ struct Blockboard generate_blockboard(void){
 			break;
 		}
 	}
+
+	printf("Added tiles to blockboard.\nGenerating agent tile.\n");
+	
+	int x = 0;
+	while(new_board.tiles[x].initialised == 1){
+		if(new_board.tiles[x].x == g_agent.x && new_board.tiles[x].y == g_agent.y){
+			printf("New blockboard rejected. Tiles and agents colide!. [(%d,%d) && (%d,%d)]\nPress enter to exit.", g_agent.x, g_agent.y, new_board.tiles[x].x, new_board.tiles[x].y);
+			getchar();
+			exit(0);
+		}
+		x += 1;
+	}
+	new_board.agent = g_agent;
 
 	return new_board;
 }
